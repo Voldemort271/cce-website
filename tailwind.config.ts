7c84c992cccbaba9b47f88643a9e293c3b802690
voldemort271 <shubhamspadhi@gmail.com>
2024-10-26 15:12:26 +0530

    Font config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        outfit: ["var(--font-outfit)"],
+        raleway: ["var(--font-raleway)"],
       },
     },
   },
