3c979f62cadbed0418d78efd06522aed710c509b
voldemort271 <shubhamspadhi@gmail.com>
2024-11-11 22:40:04 +0530

    Removed stats

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Header from "../components/home/header";
-import StatsCard from "../components/home/stats-card";
 import InfoSection from "../components/home/info-section";
 import AboutCard from "../components/home/about-card";
 import ActivitiesSection from "../components/home/activities-section";
@@ -9,8 +8,8 @@ export default function Home() {
   return (
     <div className="relative">
       <Header />
-      <StatsCard />
-      <div className="w-full py-24">
+      {/*<StatsCard />*/}
+      <div className="w-full pb-24">
         <Waves color={"#E2E8F0"} />
         <div className="bg-slate-200 relative">
           <div className="py-12 px-24 flex flex-col justify-center items-center">
