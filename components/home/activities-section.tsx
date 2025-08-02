28c2d92fe7932206f7f477f5dace4cec53888664
Divyansh Jindal <divyanshjindal95.2012@gmail.com>
2025-08-02 21:08:33 +0530

    Modifying current activity

--- components/home/activities-section.tsx
+++ components/home/activities-section.tsx
@@ -8,72 +8,32 @@ const ActivitiesSection = () => {
         <center>Current Activities</center>
       </div>
       <div className="w-full my-5 flex flex-wrap justify-center gap-5">
-
         <div className="bg-slate-300 rounded-md p-5 min-w-72">
-          <center>
-            One-month Residential Program on AI, Robotics & IoT (PRAYAS 3.0)
+          <center className="text-xl">
+            GAIN Course: Elements of Smart Structures
+          </center>
+          <center className="">
+            Prof. Vishal Singh Chauhan, SMME, IIT Mandi (December 15-20, 2025)
           </center>
           <div className="flex justify-around">
             <Link
+              href={"https://research.iitmandi.ac.in/eoss2025/"}
+              style={{ color: "blue" }}
+              target="_blank"
+            >
+              <center>Website</center>
+            </Link>
+            <Link
               href={
-                "https://www.iitmandi.ac.in/pdf/announcements/PRAYAS_3_Brochure.pdf"
+                "https://research.iitmandi.ac.in/eoss2025/files/Poster_stc.pdf"
               }
               style={{ color: "blue" }}
               target="_blank"
             >
               <center>Brochure</center>
             </Link>
-            <Link
-              href={"https://bsates.tech/prayas.html"}
-              style={{ color: "blue" }}
-              target="_blank"
-            >
-              <center>Link</center>
-            </Link>
           </div>
         </div>
-
-        <div className="bg-slate-300 rounded-md p-5 min-w-72">
-          <center>
-            A Hands-on Workshop on Composites Design and Manufacturing (16 – 20 June 2025)
-          </center>
-          <Link
-            href="https://research.iitmandi.ac.in/cdpmhm2025/doc/5D%20CDM%20HW%202025%20Brochure.pdf"
-            style={{ color: "blue" }}
-            target="_blank"
-          >
-            <center>Brochure</center>
-          </Link>
-        </div>
-
-        <div className="bg-slate-300 rounded-md p-5 min-w-72">
-          <center>
-            Masterclass on Statistical Data Analytics & Deep Learning (23 – 27 June 2025)
-          </center>
-
-          <Link
-            href={
-              "https://drive.google.com/file/d/1FJSxgLT7KQqiRbGKpTTksIgr5RIYwNg-/view?usp=sharing"
-            }
-            target={"_blank"}
-            style={{ color: "blue" }} 
-          >
-            <center>Poster</center>
-          </Link>
-
-          <Link
-            href={
-              "https://research.iitmandi.ac.in/masterclass/"
-            }
-            target={"_blank"}
-            style={{ color: "blue" }}
-          >
-            <center>Website</center>
-          </Link>
-        </div>
-
-        
-        
       </div>
     </div>
   );
